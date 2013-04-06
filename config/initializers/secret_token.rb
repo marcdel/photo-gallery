baeb9e58aeeff40e712bf294405969dc954ccf39
marcdel <delagrammatikas@gmail.com>
2013-04-06 16:12:45 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoGallery::Application.config.secret_token = 'aab95dc6a0b5f5c3069f6d457e37ebca3035901b8ffd0d11af1bc46250f509add91a44b8cacb01b7cfbe1b180323143b3aff694e7a8527a58a04c5d6d516a695'
