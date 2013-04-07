149db996cbf88bff4356d63aa42df5f52fbedc69
marcdel <delagrammatikas@gmail.com>
2013-04-07 00:26:18 -0700

    Remove section form layout. Add test for thumbnails ul element.

--- spec/views/galleries/index.html.erb_spec.rb
+++ spec/views/galleries/index.html.erb_spec.rb
@@ -9,4 +9,6 @@ describe "Galleries page" do
 
   it { should have_selector("title", text: full_title(page_title)) }
   it { should have_selector("h1", text: heading) }
+
+  it { should have_selector("ul", class: "thumbnails") }
 end
